d27777637f6344fd1b402627890555dd973c2012
Lugh Wilson <lughjw@gmail.com>
2019-12-08 19:40:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}`
+  return `Happy ${holiday}, ${name}!`
 }
+
+
